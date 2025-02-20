ed3e3e239bceb32ad7a2a9f06909a9b9445cf316
Vibhor Dubey <vibhordubey333@gmail.com>
2025-02-21 00:06:55 +0530

    LoanService|Adding update method

--- internal/repository/loan_repository.go
+++ internal/repository/loan_repository.go
@@ -5,11 +5,14 @@ import (
 	"database/sql"
 	"vibhordubey333/loan-service/internal/domain"
 	"github.com/google/uuid"
+	"encoding/json"
+	"errors"
 )
 
 type LoanRepository interface {
 	Create(ctx context.Context, loan *domain.Loan) error
 	GetByID(ctx context.Context, id uuid.UUID) (*domain.Loan, error)
+	Update(ctx context.Context, loan *domain.Loan) error
 }
 
 type loanRepository struct {
@@ -46,6 +49,57 @@ func (r *loanRepository) Create(ctx context.Context, loan *domain.Loan) error {
 
 	if err != nil {
 		return err
+	}
+
+	return tx.Commit()
+}
+
+func (r *loanRepository) Update(ctx context.Context, loan *domain.Loan) error {
+	tx, err := r.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	approvalJSON, err := json.Marshal(loan.ApprovalDetails)
+	if err != nil {
+		return err
+	}
+
+	disbursementJSON, err := json.Marshal(loan.DisbursementDetails)
+	if err != nil {
+		return err
+	}
+
+	query := `
+		UPDATE loans 
+		SET state = $1,
+			approval_details = $2,
+			disbursement_details = $3,
+			agreement_letter_url = $4,
+			updated_at = $5
+		WHERE id = $6`
+
+	result, err := tx.ExecContext(ctx, query,
+		loan.State,
+		approvalJSON,
+		disbursementJSON,
+		loan.AgreementLetterURL,
+		loan.UpdatedAt,
+		loan.ID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return errors.New("loan not found")
 	}
 
 	return tx.Commit()
